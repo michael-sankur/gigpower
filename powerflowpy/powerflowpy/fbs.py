7b33a12f6ce8dc8f582b442cd92e35f5521c6a6d
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-08-25 16:16:19 -0700

    Implement topo_sort recursively

--- powerflowpy/powerflowpy/fbs.py
+++ powerflowpy/powerflowpy/fbs.py
@@ -3,7 +3,7 @@
 # File created: 20 August 2020
 # Implement FBS to solve Power Flow for a radial distribution network.
 import sys
-from typing import Iterable, List
+from typing import Iterable, List, Dict
 from . utils import init_from_dss
 from . network import *
 
@@ -28,7 +28,7 @@ def topo_sort(network: Network) -> List:
     clock = len(network.nodes) # countdown from total number of nodes.
     # clock will be used as an index to topo_order. the strategy is to insert
     # nodes in the topo_order array in reverse order of dfs finishing times.
-    topo_order = [None] * clock  # array to store topo order
+    topo_order = [None] * clock # array to store topo order
     nodes = { node_name: 'new' for node_name in network.nodes.keys()} # initialize all nodes' statuses to 'New'
 
     # top level call to _topo_sort_dfs for each node
@@ -36,23 +36,19 @@ def topo_sort(network: Network) -> List:
     #about isolated nodes or the network is a forest of trees
     for node_name,status in nodes.items():
         if status is 'new':
-            dfs_stack = [node_name] # use python's list as a stack
-            while dfs_stack:
-                curr = dfs_stack.pop()
-                nodes[curr] = 'active'
-                clock -= 1 # decrement clock for a reverse post-order over the vertices
-                topo_order[clock] = curr
-                for child in network.adj[curr]:
-                    if nodes[child] is 'new':
-                        dfs_stack.append(child)
-                    elif nodes[child] is 'active':
-                    # child's status is 'active', meaning we have found a back edge
-                    # and there is a cycle from child to curr
-                        raise ValueError('Network contains a cycle.')
-                    # implicit else: child's status is 'finished', so we already assigned the child to topo_order
-                nodes[curr] = 'finished'
-            # when the stack is exhausted, we have finished a traversal.
+            clock = topo_sort_dfs(node_name, nodes, network.adj, clock, topo_order)
     return topo_order
+
+def topo_sort_dfs(start_node:str, node_status: Dict[str,str], adj_matrix: Iterable, clock: int, topo_order: List) -> int:
+    node_status[start_node] = 'active'
+    for child in adj_matrix[start_node]:
+        if node_status[child] is 'new':
+            clock = topo_sort_dfs(child, node_status, adj_matrix, clock, topo_order)
+        elif node_status[child] is 'active':
+            raise ValueError('Network contains a cycle.')
+    node_status[start_node] = 'finished'
+    topo_order[clock-1] = start_node
+    return clock - 1
 
 
 
