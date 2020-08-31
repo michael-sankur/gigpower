ecbcfa2051f9126079ae946a61b7e79d438684f6
Elaine Laguerta <12928553+elaguerta@users.noreply.github.com>
2020-08-31 13:55:40 -0700

    Refactor Solution to store solution vars in separate tables

--- powerflowpy/powerflowpy/solution.py
+++ powerflowpy/powerflowpy/solution.py
@@ -16,44 +16,45 @@ class Solution:
         self.Vref = np.array( [1, np.exp(1j*240*np.pi/180), np.exp(1j*120*np.pi/180)], dtype = complex)
         # hard coded VREF = [1, 1*exp(j*240*pi/180), 1*exp(j*120*pi/180)]
         #TODO: set reference voltage based on source bus
-        self.solved_nodes = dict()
-        self.solved_lines = dict()
+        self.V = dict() # 3x1 complex pu voltages phasors. Indexed by node name.
+        self.I = dict() # 3x1 complex pu current phasors. Indexed by node name.
+        self.Inode = dict() # 3x1 complex pu current phasors delivered to node. Indexed by node name.
+        self.I = dict() # 3x1 complex pu current phasors. Indexed by line key.
+        self.S = dict() # 3x1 complex pu power phasors delivered to node. Indexed by node name.
+        self.sV = dict() # 3x1 complex pu power phasors consumed by node. Indexed by node name.
         self.tolerance = -1  # stores the tolerance
         self.diff = -1  # stores the final value of Vtest - Vref at convergence
         self.root = None # keeps a pointer to the root node
 
         """ Set up voltages and currents for all nodes """
         for node in network.get_nodes():
-            node_dict = dict()
-            self.solved_nodes[node.name] = node_dict
             # set initial voltage at each node to reference voltage
-            node_dict['V'] = self.Vref
+            self.V[node.name] = self.Vref
             # initialize a zeroed out array for each solution var
-            node_dict['Inode'] = np.zeros(3, dtype='complex')
-            node_dict['S'] = np.zeros(3, dtype='complex')
-            node_dict['s'] = np.zeros(3, dtype = 'complex') # tracks voltage dependent load
+            self.Inode[node.name] = np.zeros(3, dtype='complex')
+            self.S[node.name] = np.zeros(3, dtype='complex')
+            self.s[node.name] = np.zeros(3, dtype = 'complex') # tracks voltage dependent load
+            self.sV[node.name] = np.zeros(3, dtype='complex')
+
+        """ Set up currents for all lines """
         for line in network.get_lines():
-            line_dict = dict()
-            self.solved_lines[line.key] = line_dict
-            line_dict['I'] = np.zeros(3, dtype='complex')
+            self.I[line.name] = np.zeros(3, dtype='complex')
 
     def update_voltage_forward(self, network: Network, parent: Node, child: Node) -> None:
         """
         updates voltage at child based on parent according to:
         child_node_dict['V'] = parent_dict['V'] - (parent,child).FZpu * (parent,child).I
         """
-        parent_dict = self.solved_nodes[parent.name]
-        child_dict = self.solved_nodes[child.name]
         line_key = (parent.name, child.name)
-        parent_V = parent_dict['V']
-        child_V = child_dict['V']
+        parent_V = self.V[parent.name]
+        child_V = self.V[child.name]
         FZpu = network.lines[line_key].FZpu
-        I = self.solved_lines[line_key]['I']
+        I = self.I[line_key]
 
         # child node voltage = parent node voltage - current(child_node, parent)
         new_child_V = parent_V - np.matmul(FZpu, I)
         # zero out voltages for not existent phases at child node
-        child_dict['V'] = mask_phases(new_child_V, (3,), child.phases)
+        V[ child.name ] = mask_phases(new_child_V, (3,), child.phases)
         return None
 
     def update_voltage_backward(self, network: Network, child: Node) -> None:
@@ -80,21 +81,19 @@ class Solution:
         Updates current on a line based on the downstream node. Used during backward sweep.
         """
         node_name = line_in.key[1]
-        line_dict = self.solved_lines[line_in.key]
-        node_dict = self.solved_nodes[ node_name ]
         node_phases = network.nodes[ node_name ].phases
-        line_I = line_dict['I']
-        node_s = node_dict['s']
-        node_V = node_dict['V']
+        line_I = self.I[line_in.key]
+        node_s = self.s[node_name]
+        node_V = self.V[node_name]
         new_line_I = np.conj(np.divide(node_s, node_V))
         # sum currents over all node's child segments
         for child_name in network.adj[node_name]:
             child_segment = (node_name, child_name)
-            new_line_I = new_line_I + self.solved_lines[child_segment]['I']
+            new_line_I = new_line_I + self.I[child_segment]
 
         # TODO: confirm that np.divide is the same as matlab right divide './'
         new_line_I = mask_phases(new_line_I, (3,), node_phases)
-        line_dict['I'] = new_line_I
+        self.I[line_in.key] = new_line_I
 
     def update_voltage_dependent_load(self, network: Network) -> None:
         """
@@ -107,26 +106,43 @@ class Solution:
         aI = 0
         aZ =  0
         # TODO; get aPQ, aI, aZ from dss file
-        for node_name, node_dict in self.solved_nodes.items():
-            node = network.nodes[node_name]
-            node_V = node_dict['V']
+        for node in network.get_nodes():
+            node_V = self.V[node.name]
             wpu = np.zeros(3) # TODO: get wpu from dss file
             cappu = np.zeros(3)  # TODO: get cappu from dss file
             spu = node.load.spu if node.load else np.zeros(3)
-            node_dict['s'] = np.multiply(spu, aPQ + np.multiply(aI, abs(node_V)) ) + np.multiply(aZ, (np.power(abs(node_V), 2))) - 1j * cappu + wpu
+            self.S[node_name] = np.multiply(spu, aPQ + np.multiply(aI, abs(node_V)) ) + np.multiply(aZ, (np.power(abs(node_V), 2))) - 1j * cappu + wpu
         return None
 
-    def solved_nodes_df(self) -> Iterable:
+    def V_df(self) -> Iterable:
         """
-        returns solved nodes as a dataframe indexed by node
+        returns self.V as a dataframe indexed by node name
         """
-        return pd.DataFrame.from_dict(self.solved_nodes, orient = 'index')
+        return pd.DataFrame.from_dict(self.V, orient = 'index')
 
-    def solved_lines_df(self) -> Iterable:
+    def I_df(self) -> Iterable:
         """
-        returns solved lines as a dataframe indexed by line
+        returns self.I as a dataframe indexed by line key
         """
-        return pd.DataFrame.from_dict(self.solved_lines, orient = 'index')
+        return pd.DataFrame.from_dict(self.I, orient = 'index')
+
+    def Inode_df(self) -> Iterable:
+        """
+        returns self.Inode as a dataframe indexed by node name
+        """
+        return pd.DataFrame.from_dict(self.Inode, orient='index')
+
+    def S_df(self) -> Iterable:
+        """
+        returns self.S as a dataframe indexed by node name
+        """
+        return pd.DataFrame.from_dict(self.S, orient='index')
+
+    def sV_df(self) -> Iterable:
+        """
+        returns self.sV as a dataframe indexed by node name
+        """
+        return pd.DataFrame.from_dict(self.sV, orient='index')
 
     def params_df(self) -> Iterable:
         """
@@ -142,10 +158,20 @@ class Solution:
         """
         print("Parameters:")
         print(self.params_df())
-        print("Solved Nodes:")
-        for key,d in self.solved_nodes.items():
-            print(f"{key} \t {d['V']}")
-        print("Solved Lines:")
-        print(self.solved_lines_df())
+
+        print("V solution")
+        print(self.V_df())
+
+        print("I solution")
+        print(self.I_df())
+
+        print("Inode solution")
+        print(self.Inode_df())
+
+        print("S solution")
+        print(self.S_df())
+
+        print("sV solution")
+        print(self.sV_df())
         print()
 
