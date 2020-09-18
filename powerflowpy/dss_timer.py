277dfd8c3bc09c7aeaf518d3ef019bddefad35d7
Elaine Laguerta <ellaguerta@lbl.gov>
2020-09-18 14:21:41 -0700

    Add dss detailed timing

---
+++ powerflowpy/dss_timer.py
@@ -0,0 +1,27 @@
+from powerflowpy.dss_solve import *
+import numpy as np
+import csv
+
+dss_file = 'powerflowpy/tests/06n3ph_rad_unbal/06node_threephase_radial_unbalanced.dss'
+out_csv = './powerflowpy/tests/timings/dss_detailed_timing.csv'
+TRIALS = 20
+if __name__ == '__main__':
+    with open(out_csv, 'w') as out:
+        writer = csv.writer(out)
+        writer.writerow(
+            [f'TIMING DATA FOR DSS. INPUT FILE: {dss_file}. All timing data in ms.'])  # title
+        # column headers
+        writer.writerow(['num_iterations', 'total_time', 'init_circuit_time', 'save_loads_time',
+                         'reset_loads_total_time', 'run_solvesnap_total_time', 'final_calcs_time'])
+
+        for i in range(TRIALS):
+            result = np.zeros(7) # initialize result row for this trial set
+            for j in range(i+1):
+                trial_result = solve_with_dss(dss_file)
+                total_time = sum(trial_result)
+                # add this trial to the result array
+                result = result + np.asarray([1, total_time] + trial_result)
+            writer.writerow(result)
+
+
+
--- powerflowpy/powerflowpy/dss_solve.py
+++ powerflowpy/powerflowpy/dss_solve.py
@@ -6,8 +6,11 @@
 import opendssdirect as dss
 import numpy as np
 import pandas as pd
+import time
 
 def solve_with_dss(dss_file):
+    # INITIALIZE DSS CIRCUIT----------------------------------------------------
+    t1 = time.perf_counter()
     dss.run_command('Redirect ' + dss_file)
 
     # Set slack bus (sourcebus) voltage reference in p.u.
@@ -19,7 +22,10 @@ def solve_with_dss(dss_file):
     Sbase = 1000000.0
     Ibase = Sbase/Vbase
     Zbase = Vbase/Ibase
+    t2 = time.perf_counter()
+    init_circuit_time = (t2 - t1) * 1000
 
+    # SAVE LOADS----------------------------------------------------------
 
     # Solve power flow with OpenDSS file, and updating loads at each timestep.
     # Code based on https://sourceforge.net/p/electricdss/code/HEAD/tree/trunk/Version8/Distrib/Examples/Python/Python-to-OpenDSS%20Control%20Interface.pdf
@@ -29,22 +35,27 @@ def solve_with_dss(dss_file):
     # save original loads
     orig_loads_data = dss.utils.loads_to_dataframe()
     orig_loads_data = orig_loads_data.transpose()
-    # print original loads
-    print("OpenDSS Loads before solving, from dss.CktElement.Powers():")
-    print(dss.CktElement.Powers())
+    t3 = time.perf_counter()
+    save_loads_time = (t3 - t2) * 1000
+
+    reset_loads_total_time = 0
+    run_solvesnap_total_time = 0
 
     for stepNumber in range(originalSteps): # simulate timesteps
-
+        t4 = time.perf_counter()
         # set loads
         for load_name in dss.Loads.AllNames():
             load_data = orig_loads_data[load_name]
             dss.Loads.Name(load_name)
             dss.Loads.kW( load_data['kW'] )
             dss.Loads.kvar( load_data['kvar'] )
-
+        t5 = time.perf_counter()
+        reset_loads_total_time += (t5 - t4) * 1000
         #run solve for this timestep
         dss.Solution.SolveSnap()
         dss.Solution.FinishTimeStep()
+        t6 = time.perf_counter()
+        run_solvesnap_total_time += (t6 - t5) * 1000
 
     if not dss.Solution.Converged:
         print('Initial Solution Not Converged. Check Model for Convergence')
@@ -60,8 +71,6 @@ def solve_with_dss(dss_file):
         dss.Solution.ControlMode(-1)
         dss.Solution.MaxControlIterations(1000000)
         dss.Solution.MaxIterations(30000)
-
-        print(f'\nOpendss Iterations: {dss.Solution.Iterations()}\tOpendss Convergence: {dss.Solution.Convergence()}')
 
         VDSS = np.zeros((len(dss.Circuit.AllBusNames()), 3), dtype='complex')
         for k1 in range(len(dss.Circuit.AllBusNames())):
@@ -99,6 +108,7 @@ def solve_with_dss(dss_file):
 
         dssStx = pd.DataFrame(STXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
         dssSrx = pd.DataFrame(SRXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
-        print("OpenDSS Loads after solving, from dss.CktElement.Powers():")
-        print(dss.CktElement.Powers())
-        return dssV, dssI, dssStx, dssSrx
+
+        t7 = time.perf_counter()
+        final_calcs_time = (t7 - t6) * 1000
+        return [init_circuit_time, save_loads_time, reset_loads_total_time, run_solvesnap_total_time, final_calcs_time]
