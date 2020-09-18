3f2f79eae0f7c5197f4c73dd70dc3d3fdd537930
Elaine Laguerta <ellaguerta@lbl.gov>
2020-09-18 15:10:58 -0700

    Add final calcs to opendss solve with constant loads

--- powerflowpy/powerflowpy/dss_solve.py
+++ powerflowpy/powerflowpy/dss_solve.py
@@ -134,3 +134,48 @@ def solve_with_dss_constant_loads(dss_file):
         dss.Solution.MaxControlIterations(1000000)
         dss.Solution.MaxIterations(30000)
         dss.Solution.Cleanup()
+
+        print(
+            f'\nOpendss Iterations: {dss.Solution.Iterations()}\tOpendss Convergence: {dss.Solution.Convergence()}')
+
+        VDSS = np.zeros((len(dss.Circuit.AllBusNames()), 3), dtype='complex')
+        for k1 in range(len(dss.Circuit.AllBusNames())):
+            dss.Circuit.SetActiveBus(dss.Circuit.AllBusNames()[k1])
+            ph = np.asarray(dss.Bus.Nodes(), dtype='int')-1
+            Vtemp = np.asarray(dss.Bus.PuVoltage())
+            Vtemp = Vtemp[0:5:2] + 1j*Vtemp[1:6:2]
+            VDSS[k1, ph] = Vtemp
+        dssV = pd.DataFrame(VDSS, dss.Circuit.AllBusNames(), ['A', 'B', 'C'])
+
+        IDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
+        for k1 in range(len(dss.Lines.AllNames())):
+            dss.Lines.Name(dss.Lines.AllNames()[k1])
+
+            ph = np.asarray(dss.CktElement.BusNames()[
+                            0].split('.')[1:], dtype='int')-1
+            Imn = np.asarray(dss.CktElement.Currents())/Ibase
+            Imn = Imn[0:int(len(Imn)/2)]
+            Imn = Imn[0:5:2] + 1j*Imn[1:6:2]
+            IDSS[k1, ph] = Imn
+        dssI = pd.DataFrame(IDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
+
+        STXDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
+        SRXDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
+
+        for k1 in range(len(dss.Lines.AllNames())):
+            dss.Lines.Name(dss.Lines.AllNames()[k1])
+            ph = np.asarray(dss.CktElement.BusNames()[
+                            0].split('.')[1:], dtype='int')-1
+            Sk = np.asarray(dss.CktElement.Powers())/(Sbase/1000)
+            STXtemp = Sk[0:int(len(Sk)/2)]
+            SRXtemp = Sk[int(len(Sk)/2):]
+            STXtemp = STXtemp[0:5:2] + 1j*STXtemp[1:6:2]
+            SRXtemp = -(SRXtemp[0:5:2] + 1j*SRXtemp[1:6:2])
+            STXDSS[k1, ph] = STXtemp
+            SRXDSS[k1, ph] = SRXtemp
+
+        dssStx = pd.DataFrame(STXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
+        dssSrx = pd.DataFrame(SRXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
+        print("OpenDSS Loads after solving, from dss.CktElement.Powers():")
+        print(dss.CktElement.Powers())
+        return dssV, dssI, dssStx, dssSrx
