cd2d6b77f968ac09434e09621c3217acf43d8a85
Elaine Laguerta <ellaguerta@lbl.gov>
2020-10-01 12:29:22 -0700

    Remove final solution calculations form solve_with_dss Make a new helper method, dss_solve.get_solution() that calculates and returns solution vars.

--- powerflowpy/powerflowpy/dss_solve.py
+++ powerflowpy/powerflowpy/dss_solve.py
@@ -8,18 +8,17 @@ import numpy as np
 import pandas as pd
 
 def solve_with_dss(dss_file):
+    """
+    Initialize the network with a dss file, with one call to 'Redirect'.
+    Set loads on the network.
+    Call SolveSnap() to solve powerflow for one timestep.
+    """
     dss.run_command('Redirect ' + dss_file)
 
     # Set slack bus (sourcebus) voltage reference in p.u.
     SlackBusVoltage = 1.000
     dss.Vsources.PU(SlackBusVoltage)
     dss.Solution.Convergence(0.000000000001)
-
-    Vbase = dss.Bus.kVBase() * 1000
-    Sbase = 1000000.0
-    Ibase = Sbase/Vbase
-    Zbase = Vbase/Ibase
-
 
     # Solve power flow with OpenDSS file, and updating loads before solving once
     # Code based on https://sourceforge.net/p/electricdss/code/HEAD/tree/trunk/Version8/Distrib/Examples/Python/Python-to-OpenDSS%20Control%20Interface.pdf
@@ -58,48 +57,56 @@ def solve_with_dss(dss_file):
             #go under a reset process
             dss.Monitors.ResetAll()
 
+def get_solution():
+    """
+    Input: A dss object with a solved network.
+    Output: Network solution paramters V, I, Stx, Srx in pandas DataFrames.
+    Also prints iterations and convergence to stdout∏.
+    """
+        print(f'\nOpendss Iterations: {dss.Solution.Iterations()}\tOpendss Convergence: {dss.Solution.Convergence()}')
+        Vbase = dss.Bus.kVBase() * 1000
+        Sbase = 1000000.0
+        Ibase = Sbase/Vbase
+        Zbase = Vbase/Ibase
 
+        VDSS = np.zeros((len(dss.Circuit.AllBusNames()), 3), dtype='complex')
+        for k1 in range(len(dss.Circuit.AllBusNames())):
+            dss.Circuit.SetActiveBus(dss.Circuit.AllBusNames()[k1])
+            ph = np.asarray(dss.Bus.Nodes(), dtype='int')-1
+            Vtemp = np.asarray(dss.Bus.PuVoltage())
+            Vtemp = Vtemp[0:5:2] + 1j*Vtemp[1:6:2]
+            VDSS[k1, ph] = Vtemp
 
-        # print(f'\nOpendss Iterations: {dss.Solution.Iterations()}\tOpendss Convergence: {dss.Solution.Convergence()}')
+        dssV = pd.DataFrame(VDSS, dss.Circuit.AllBusNames(), ['A', 'B', 'C'])
 
-        # VDSS = np.zeros((len(dss.Circuit.AllBusNames()), 3), dtype='complex')
-        # for k1 in range(len(dss.Circuit.AllBusNames())):
-        #     dss.Circuit.SetActiveBus(dss.Circuit.AllBusNames()[k1])
-        #     ph = np.asarray(dss.Bus.Nodes(), dtype='int')-1
-        #     Vtemp = np.asarray(dss.Bus.PuVoltage())
-        #     Vtemp = Vtemp[0:5:2] + 1j*Vtemp[1:6:2]
-        #     VDSS[k1, ph] = Vtemp
+        IDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
+        for k1 in range(len(dss.Lines.AllNames())):
+            dss.Lines.Name(dss.Lines.AllNames()[k1])
 
-        # dssV = pd.DataFrame(VDSS, dss.Circuit.AllBusNames(), ['A', 'B', 'C'])
+            ph = np.asarray(dss.CktElement.BusNames()[0].split('.')[1:], dtype='int')-1
+            Imn = np.asarray(dss.CktElement.Currents())/Ibase
+            Imn = Imn[0:int(len(Imn)/2)]
+            Imn = Imn[0:5:2] + 1j*Imn[1:6:2]
+            IDSS[k1,ph] = Imn
+        dssI = pd.DataFrame(IDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
 
-        # IDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
-        # for k1 in range(len(dss.Lines.AllNames())):
-        #     dss.Lines.Name(dss.Lines.AllNames()[k1])
+        STXDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
+        SRXDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
 
-        #     ph = np.asarray(dss.CktElement.BusNames()[0].split('.')[1:], dtype='int')-1
-        #     Imn = np.asarray(dss.CktElement.Currents())/Ibase
-        #     Imn = Imn[0:int(len(Imn)/2)]
-        #     Imn = Imn[0:5:2] + 1j*Imn[1:6:2]
-        #     IDSS[k1,ph] = Imn
-        # dssI = pd.DataFrame(IDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
+        for k1 in range(len(dss.Lines.AllNames())):
+            dss.Lines.Name(dss.Lines.AllNames()[k1])
+            ph = np.asarray(dss.CktElement.BusNames()[0].split('.')[1:], dtype='int')-1
+            Sk = np.asarray(dss.CktElement.Powers())/(Sbase/1000)
+            STXtemp = Sk[0:int(len(Sk)/2)]
+            SRXtemp = Sk[int(len(Sk)/2):]
+            STXtemp = STXtemp[0:5:2] + 1j*STXtemp[1:6:2]
+            SRXtemp = -(SRXtemp[0:5:2] + 1j*SRXtemp[1:6:2])
+            STXDSS[k1, ph] = STXtemp
+            SRXDSS[k1, ph] = SRXtemp
 
-        # STXDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
-        # SRXDSS = np.zeros((len(dss.Lines.AllNames()), 3), dtype='complex')
-
-        # for k1 in range(len(dss.Lines.AllNames())):
-        #     dss.Lines.Name(dss.Lines.AllNames()[k1])
-        #     ph = np.asarray(dss.CktElement.BusNames()[0].split('.')[1:], dtype='int')-1
-        #     Sk = np.asarray(dss.CktElement.Powers())/(Sbase/1000)
-        #     STXtemp = Sk[0:int(len(Sk)/2)]
-        #     SRXtemp = Sk[int(len(Sk)/2):]
-        #     STXtemp = STXtemp[0:5:2] + 1j*STXtemp[1:6:2]
-        #     SRXtemp = -(SRXtemp[0:5:2] + 1j*SRXtemp[1:6:2])
-        #     STXDSS[k1, ph] = STXtemp
-        #     SRXDSS[k1, ph] = SRXtemp
-
-        # dssStx = pd.DataFrame(STXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
-        # dssSrx = pd.DataFrame(SRXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
-        # print("OpenDSS Loads after solving, from dss.CktElement.Powers():")
-        # print(dss.CktElement.Powers())
-        # return dssV, dssI, dssStx, dssSrx
-        # return VDSS
+        dssStx = pd.DataFrame(STXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
+        dssSrx = pd.DataFrame(SRXDSS, dss.Lines.AllNames(), ['A', 'B', 'C'])
+        print("OpenDSS Loads after solving, from dss.CktElement.Powers():")
+        print(dss.CktElement.Powers())
+        return dssV, dssI, dssStx, dssSrx
+        return VDSS
