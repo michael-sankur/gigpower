9c1a5ee40e18cea68df6ec2e1520ca600463b088
Elaine Laguerta <ellaguerta@lbl.gov>
2020-12-02 17:23:52 -0800

    Redo comparison tests for 5, 6, and 13 nodes

--- fbs/tests/test_compare_opendss.py
+++ fbs/tests/test_compare_opendss.py
@@ -15,11 +15,11 @@ import pytest
 # dss_files = ['fbs/tests/05n3ph_unbal/compare_opendss_05node_threephase_unbalanced_oscillation_03.dss',
 #               'fbs/tests/06n3ph_unbal/06node_threephase_unbalanced.dss',
 #               'fbs/tests/06n3ph_rad_unbal/06node_threephase_radial_unbalanced.dss']
-# dss_files = ['fbs/tests/06n3ph_unbal/06node_threephase_unbalanced.dss']
+dss_files = ['fbs/tests/06n3ph_unbal/06node_threephase_unbalanced.dss']
 # dss_files = ['fbs/tests/test_cases_dss/02node_threephase_unbalanced.dss']
 # dss_files = ['fbs/tests/IEEE_13_bus/IEEE_13_Bus_allwye_noxfm_noreg.dss']
 # dss_files = ['fbs/tests/IEEE_13_bus/IEEE_13_Bus_original.dss']
-dss_files = ['fbs/tests/IEEE_13_bus/IEEE_13_Bus_allwye.dss']
+# dss_files = ['fbs/tests/IEEE_13_bus/IEEE_13_Bus_allwye.dss']
 
 
 def test_all():
@@ -80,9 +80,9 @@ def compare_dfs(fbs_df : pd.DataFrame, dss_df : pd.DataFrame) -> None:
     pd.options.display.float_format = '{:.4f}'.format
     print("Max |diff|:")
     print(compare.abs().max())
-    print(compare,"\n")
+    print(compare, "\n")
     print("FBS results")
-    print(fbs_df,"\n")
+    print(fbs_df, "\n")
     print("DSS results")
     print(dss_df)
     return compare.abs().max()
