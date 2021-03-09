ed729ef9e4195eb150c7f14d8ee652ae73c99209
Elaine Laguerta <ellaguerta@lbl.gov>
2021-03-09 11:02:41 -0800

    Fix nr3 mapping of TXnode and RXnode

--- nr3/nr3/lib/relevant_openDSS_parameters.py
+++ nr3/nr3/lib/relevant_openDSS_parameters.py
@@ -8,7 +8,7 @@ def relevant_openDSS_parameters(fn, t):
 
     tf_no = len(dss.Transformers.AllNames()) - len(dss.RegControls.AllNames()) #number of transformers
     vr_no = len(dss.RegControls.AllNames()) #number of voltage regulators
-    
+
     dss.Circuit.SetActiveBus(dss.Circuit.AllBusNames()[0])
 
     #BASE values
@@ -26,26 +26,26 @@ def relevant_openDSS_parameters(fn, t):
     for tf in range(len(dss.Transformers.AllNames())):
         dss.Transformers.Name(dss.Transformers.AllNames()[tf])
         for i in range(2):
-            if dss.Transformers.AllNames()[tf] in dss.RegControls.AllNames():     
+            if dss.Transformers.AllNames()[tf] in dss.RegControls.AllNames():
                 bus = dss.CktElement.BusNames()[i].split('.')
-                vr_bus[i, vr_count] = int(dss.Circuit.AllBusNames().index(bus[0]))    
+                vr_bus[i, vr_count] = int(dss.Circuit.AllBusNames().index(bus[0]))
                 vr_bus[2, vr_count] = int(bus[1]) #phase
-                whichone = 0            
+                whichone = 0
             else:
-                tf_bus[i, tf_count] =  int(dss.Circuit.AllBusNames().index(dss.CktElement.BusNames()[i])) #stuff the in and out bus of the tf into an array          
+                tf_bus[i, tf_count] =  int(dss.Circuit.AllBusNames().index(dss.CktElement.BusNames()[i])) #stuff the in and out bus of the tf into an array
                 whichone = 1
         if whichone == 1:
             tf_count += 1
         else:
             vr_count += 1
         whichone = -1
-    
+
     # vr_no = 1
     # vr_bus = vr_bus[0:2, 0:1]
 
     nline = len(dss.Lines.AllNames()) + tf_no + (2* vr_no)
     nnode = len(dss.Circuit.AllBusNames())
-   
+
     line_in_idx_vr = range(len(dss.Lines.AllNames()) + tf_no, nline, 2)
     line_out_idx_vr = range(len(dss.Lines.AllNames()) + tf_no + 1, nline, 2)
 
@@ -66,12 +66,10 @@ def relevant_openDSS_parameters(fn, t):
     #TXnum and RXnum
     for line in range(len(dss.Lines.AllNames())):
         dss.Lines.Name(dss.Lines.AllNames()[line]) #set the line
-        bus1 = dss.Lines.Bus1()
-        bus2 = dss.Lines.Bus2()
-        pattern = r"(\w+)." #this appears to wrok
-
-        TXnode[line] = re.findall(pattern, bus1)[0]
-        RXnode[line] = re.findall(pattern, bus2)[0]
+        bus1 = dss.Lines.Bus1().split('.')[0]
+        bus2 = dss.Lines.Bus2().split('.')[0]
+        TXnode[line] = bus1
+        RXnode[line] = bus2
         TXnum[line] = dss.Circuit.AllBusNames().index(TXnode[line])
         RXnum[line] = dss.Circuit.AllBusNames().index(RXnode[line])
 
@@ -87,7 +85,7 @@ def relevant_openDSS_parameters(fn, t):
     for line in range(len(line_in_idx_vr)):
         print('is there a voltage regulator?')
         lineinidx = line_in_idx_vr[line]
-        
+
         TXnode[lineinidx] = dss.Circuit.AllBusNames()[vr_bus[0, line]]
         RXnode[lineinidx] = dss.Circuit.AllBusNames()[vr_bus[1, line]]
         TXnum[lineinidx] = vr_bus[0, line]
@@ -95,12 +93,12 @@ def relevant_openDSS_parameters(fn, t):
     #VR out
     for line in range(len(line_out_idx_vr)):
         print('is there a voltage regulator part 2?')
-        lineoutidx = line_out_idx_vr[line]    
+        lineoutidx = line_out_idx_vr[line]
         TXnode[lineoutidx] = dss.Circuit.AllBusNames()[vr_bus[0, line]]
         RXnode[lineoutidx] = dss.Circuit.AllBusNames()[vr_bus[1, line]]
         TXnum[lineoutidx] = vr_bus[0, line]
         RXnum[lineoutidx] = vr_bus[1, line]
-        
+
     #spu, apq, ai, az
     spu = np.zeros((3,nnode))
     ppu = np.zeros((3,nnode))
