877bb763090480b62d0fd4b59cf4a40f172661d9
Elaine Laguerta <ellaguerta@lbl.gov>
2021-03-23 10:59:04 -0700

    Add ndarrays to Solution class, for storing solved values

--- src/circuit_mapper/solution.py
+++ src/circuit_mapper/solution.py
@@ -10,6 +10,8 @@ import opendssdirect as dss
 
 import re
 import numpy as np
+from typing import Dict
+import pandas as pd
 
 
 class Solution():
@@ -19,21 +21,69 @@ class Solution():
     SLACKIDX = 0  # assume slack bus is at index 0
     VSLACK = np.array([1, np.exp(1j*-120*np.pi/180), np.exp(1j*120*np.pi/180)])
     V0, I0 = None, None
+
+    # # TODO: Make a better 'solution.set_tolerance(ref_node, error)' method
+    # set tolerance with phase B reference voltage
+    # tolerance = abs((solution.Vref[1]) * 10**-9)
     tolerance = 1e-9
     maxiter = 100
     ZIP_V = [0.10, 0.05, 0.85, 0.10, 0.05, 0.85, 0.80]
+
+    # standardize solution parameter name, index values, columns, and 
+    # datatypes across the class
+    # see self._init_solution_matrices
+    SOLUTION_PARAMS = {
+        'V': ['buses', 3, complex],
+        'I': ['lines', 3, complex],
+        'Inode': ['buses', 3, complex],
+        'Stx': ['lines', 3, complex],
+        'Srx': ['lines', 3,  complex],
+        'sV': ['buses ', 3, complex]}
 
     def __init__(self, dss_fp: str):
         """
         sets up a Solution object with a pointer to a Circuit mapped from opendss
         Solutions keep a pointer to the dss object used to map the Circuit
         """
+        #  setup calls to opendss----------------------------------------------
         self.dss = dss.run_command('Redirect ' + dss_fp)
         dss.Solution.Solve()  # solve first for base values
-        set_zip_values(dss, self.__class__.ZIP_V)  # set zip values before mapping Circuit!
+        set_zip_values(dss, self.__class__.ZIP_V)
         dss.Solution.Solve()  # solve again to set zip values
+
+        #  map Circuit and vvc objects-----------------------------------------
         self.circuit = Circuit(dss)
         self.volt_var_controllers = self.parse_vvc_objects(dss_fp)
+
+        #  initialize solution parameters---------------------------------------
+        self.iterations = 0
+        # stores the tolerance at most recent completed iteration
+        self.solution_tolerance = -1
+        # stores the final value of Vtest - Vref at convergence
+        self.convergence_diff = -1  
+        self._init_solution_matrices()
+
+        # Voltage parameters. TODO: are these only for fbs?
+        # If so, move to solution_fbs.py
+        self.Vtest = np.zeros(3, dtype='complex')
+        self.Vref = np.array(
+            [1, np.exp(1j*240*np.pi/180), np.exp(1j*120*np.pi/180)], dtype=complex)
+
+    def _init_solution_matrices(self):
+        """
+        Initializes matrices to store solution values in ndarrays as follows:
+        V: num_buses x 3, complex pu voltage, by bus index
+        I: num_lines x 3, complex pu current phasors, by line index
+        Inode: num_buses x 3, complex pu current phasors delivered to bus, by bus index
+        Stx: num_lines x 3, line transmitting end power, by line index
+        Srx: num_lines x 3, line receiving end power, by line index
+        sV: num_buses x 3, total powers at each bus, by bus index
+        """
+        for param in self.__class__.SOLUTION_PARAMS:
+            element_group, num_cols, datatype = SOLUTION_PARAMS[param]
+            num_rows = getattr(getattr(self.circuit, element_group), 'num_elements')
+            setattr(self, param, np.zeros((num_rows, num_cols), dtype=datatype)
+
 
     def parse_vvc_objects(self, fn: str):
         """ From 20180601/PYTHON/lib/dss_vvc.py by @kathleenchang"""
