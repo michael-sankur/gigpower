ad86837fa2c1def21e4c1598f4667f493459bdff
Elaine Laguerta <ellaguerta@lbl.gov>
2021-03-23 23:12:05 -0700

    Fix pretty_print.compare_data_frames to take labels as args

--- src/circuit_mapper/pretty_print.py
+++ src/circuit_mapper/pretty_print.py
@@ -2,23 +2,26 @@ import pandas as pd
 import numpy as np
 
 
-def compare_dfs(fbs_df: pd.DataFrame, dss_df: pd.DataFrame, title: str) -> None:
-    """ helper method to compare fbs vs. dss and print comparisons """
-    compare = fbs_df - dss_df
-    pd.options.display.float_format = '{:.4f}'.format
+def compare_data_frames(df1: pd.DataFrame, df2: pd.DataFrame, df1_label:str, 
+                        df2_label: str, title: str, cols = ['A', 'B', 'C']):
+    """ 
+    prints a comparison of 2 dataframes, df1 - df2
+    """
+    summary_labels = ['MAX', 'SUM', 'MEAN']
+    compare = df1 - df2
+    col_suffix = f'{df1_label} - {df2_label}'
+    pd.options.display.float_format = '{:.2f}'.format
     if len(compare.axes) == 2:
-        compare_cols = ['A.(fbs - dss)', 'B.(fbs - dss)', 'C.(fbs - dss)']
-        compare.columns = compare_cols
+        compare.columns = [f'{col}.{col_suffix}' for col in cols]
         print(f"{title} - SUMMARY STATS")
         summary_stats = np.asarray([compare.abs().max(), compare.abs().sum(), compare.abs().mean()])
         summary = pd.DataFrame(summary_stats,
-            ['MAX |fbs - dss|', 'SUM |fbs - dss|', 'MEAN |fbs - dss|'], ['A', 'B', 'C'])
+            [f'{stat} |{col_suffix}|' for stat in summary_labels], cols)
         print(summary, '\n')
-    print(f"{title} - ERROR, |fbs - dss|:")
+    print(f"{title} - ERROR, |{col_suffix}|:")
     print(compare, "\n")
-    print(f"{title} - FBS RESULTS")
-    print(fbs_df, "\n")
-    print(f"{title} - DSS RESULTS")
-    print(dss_df, "\n")
+    print(f"{title} - {df1_label} Results")
+    print(df1, "\n")
+    print(f"{title} - {df2_label} RESULTS")
+    print(df2, "\n")
     print("~"*100, "\n")
-    return compare.abs().max()
