bc99065bfb3c9fde5d028225936a45e54ac09ff1
Elaine Laguerta <ellaguerta@lbl.gov>
2021-03-24 21:04:26 -0700

    Make dss v fbs tests fancy too

--- tests/test_dss_compare_fbs.py
+++ tests/test_dss_compare_fbs.py
@@ -14,79 +14,73 @@ import numpy as np
 import sys
 import os
 from pathlib import Path
-import requests
 import pytest
 
-DSS_FILE_REPO = 'https://raw.githubusercontent.com/lbnl-cybersecurity/python-powerflow/master/'
-OUT_DIR = Path('./tests/test_compare_results')
+DSS_FILE_DIR = Path('./src/nr3_python/')
+OUT_DIR = Path('./tests/test_compare_fbs_dss')
+OUT_PREFIX = 'FBS_v_DSS_'
 
-# dictionaries with FILE_STEM: API TOKEN
-# API token is used to download source files from raw.githubuser.com
-DSS_CKT_FILES = {
-                'IEEE_13_Node/IEEE_13_Bus_allwye.dss': 'ADCUMKIJTZZUEN5R3OXNX3DAMQCHY',
-                # 'IEEE_13_Node/IEEE_13_Bus_allwye_noxfm_noreg.dss': 'ADCUMKM27O7EDMUSGZZXHEDAIF2EQ',
-                # 'IEEE_34_feeder_UB/IEEE_34_Bus_allwye.dss': 'ADCUMKPSDEI4H6FJ64E3WP3AIF2V6',
-                # 'IEEE_34_feeder_UB/IEEE_34_Bus_allwye_noxfm_noreg.dss': 'ADCUMKKKHOWAGZAFIYF74K3AIF2ZG',
-                # 'IEEE_37_feeder_UB/IEEE_37_Bus_allwye.dss': 'ADCUMKMRXJ6XFKXF2MPS4OTAIF3BE',
-                # 'IEEE_37_feeder_UB/IEEE_37_Bus_allwye_noxfm_noreg.dss': 'ADCUMKP36GFYRQDGQKDLZK3AIF3FA'
-                }
-DSS_CONFIG_FILES = {
-                    'IEEE_13_Node/IEEELineCodes.dss': 'ADCUMKNWINSZLV323HRWDSTAMQCPQ',
-                    'IEEE_13_Node/IEEE13Node_BusXY.csv': 'ADCUMKLAYMADVC4CEN5PFP3AMQCVK',
-                    # 'IEEE_34_feeder_UB/IEEE34_BusXY.csv': 'ADCUMKPOJ2UU4GZUNGDS6B3AIF2NC',
-                    # 'IEEE_34_feeder_UB/IEEELineCodes.dss': 'ADCUMKMONSF4CEDEPYC2RUTAIF2RG',
-                    # 'IEEE_37_feeder_UB/IEEELineCodes.dss': 'ADCUMKNPPPEQ3RYI6MA7CK3AIF26M'
-                    }
+DSS_FILES = [
+                'IEEE_13_Bus_allwye.dss',
+                'IEEE_13_Bus_allwye_noxfm_noreg.dss',
+                'IEEE_34_Bus_allwye.dss',
+                'IEEE_34_Bus_allwye_noxfm_noreg.dss',
+                'IEEE_37_Bus_allwye.dss',
+                'IEEE_37_Bus_allwye_noxfm_noreg.dss'
+]
 
+GENEROUS = 1e-1
+STRICT = 1e-2
 
-def test_all():
+@pytest.fixture
+def dss_solution(dss_file):
+    fp = str(Path(DSS_FILE_DIR, dss_file))
+    dss_solution = SolutionDSS(str(fp))
+    dss_solution.solve()
+    return dss_solution
+
+@pytest.fixture
+def new_fbs_solution(dss_file):
+    fp = str(Path(DSS_FILE_DIR, dss_file))
+    solution = SolutionFBS(fp)
+    solution.solve()
+    return solution
+
+def setup_module():
+    if not OUT_DIR.exists():
+        os.makedirs(OUT_DIR)
+
+@pytest.mark.parametrize(
+    "dss_file,tolerance",
+    [
+        ('IEEE_13_Bus_allwye.dss', GENEROUS),
+        ('IEEE_13_Bus_allwye_noxfm_noreg.dss', STRICT),
+        ('IEEE_34_Bus_allwye.dss', GENEROUS),
+        ('IEEE_34_Bus_allwye_noxfm_noreg.dss', STRICT),
+        ('IEEE_37_Bus_allwye.dss', GENEROUS),
+        ('IEEE_37_Bus_allwye_noxfm_noreg.dss', GENEROUS)
+    ]
+
+)
+@pytest.mark.parametrize(
+    "solution_param",
+    [(param) for param in SolutionFBS.SOLUTION_PARAMS]
+)
+def test_dss_v_new_fbs(new_fbs_solution, dss_solution, solution_param, 
+    dss_file, tolerance):
     """
     Compare the python FBS solution to the opendss solution.
     Writes output to OUTPUT FOLDER.
     """
-    tolerance = 10**-1
-
-    # download all files
-    for file, token in DSS_CKT_FILES.items():
-        if not get_local_source_fp(file).exists():
-            download_file(file, token)
-    for file, token in DSS_CONFIG_FILES.items():
-        if not get_local_source_fp(file).exists():
-            download_file(file, token)
-
-    # test and compare all files
-    if not OUT_DIR.exists():
-        os.makedirs(OUT_DIR)
-
-    for file in DSS_CKT_FILES:
-        infile = get_local_source_fp(file)
-        out_file = Path(OUT_DIR, infile.stem).with_suffix('.out.txt')
-        sys.stdout = open(out_file, 'w')
-        compare_solution(str(infile), tolerance)
-
-
-def compare_solution(dss_file, tolerance):
-    fbs_solution = SolutionFBS(dss_file)
-    fbs_solution.solve()
-
-    dss_solution = SolutionDSS(dss_file)
-    dss_solution.solve()
-
-    for param in SolutionFBS.SOLUTION_PARAMS:
-        fbs_vals = fbs_solution.get_data_frame(param)
-        dss_vals = dss_solution.get_data_frame(param)
-        compare_data_frames(fbs_vals, dss_vals, 'fbs', 'dss', param)
-        assert ((fbs_vals - dss_vals).abs().max() <= tolerance).all() 
-
-
-def get_local_source_fp(file: str):
-    return Path(OUT_DIR, 'source_files', file)
-
-def download_file(file: str, token: str):
-    url = f'{DSS_FILE_REPO}{file}?token={token}'
-    download = requests.get(url).text
-    local_fp = get_local_source_fp(file)
-    if not local_fp.parent.exists():
-        os.makedirs(local_fp.parent)
-    f = open(get_local_source_fp(file), 'w')
-    f.write(download)
+    fp = Path(DSS_FILE_DIR, dss_file)
+    out_file = Path(OUT_DIR, solution_param + '_' + OUT_PREFIX + str(fp.stem)).with_suffix('.out.txt')
+    sys.stdout = open(out_file, 'w')
+    fbs_vals = new_fbs_solution.get_data_frame(solution_param)
+    dss_vals  = dss_solution.get_data_frame(solution_param)
+    test = ((fbs_vals - dss_vals).abs().max() <= tolerance).all()
+    if test:
+        print("TEST PASSED")
+    else:
+        print("TEST FAILED")
+    compare_data_frames(fbs_vals, dss_vals, 'fbs', 'dss', solution_param)
+    assert test
