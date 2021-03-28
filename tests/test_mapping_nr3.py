5c9ddd4d70adb7cc81ca354d8fbda215186cfad6
Elaine Laguerta <ellaguerta@lbl.gov>
2021-03-28 16:25:15 -0700

    Remove unused imports from test_mapping_nr3

--- tests/test_mapping_nr3.py
+++ tests/test_mapping_nr3.py
@@ -4,12 +4,9 @@
 # Test refactor of nr3 and fbs modules
 
 import numpy as np
-import opendssdirect as dss
 import pytest
 
-from circuit_mapper.circuit import Circuit
 from circuit_mapper.solution_nr3 import SolutionNR3
-from circuit_mapper.solution import Solution
 
 # current nr3 dependencies
 from nr3_python.lib.DSS_parameters import relevant_openDSS_parameters
