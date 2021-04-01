7acb9eb71dc5ba1bcb964698eec7f3231ebe7a3a
Elaine Laguerta <ellaguerta@lbl.gov>
2021-04-01 04:37:37 -0700

    Remove get_cappu_array from capacitor group We may want to add this back later as an alias for circuit.get_cappu_matrix

--- src/circuit_mapper/capacitor_group.py
+++ src/circuit_mapper/capacitor_group.py
@@ -5,10 +5,3 @@ from . capacitor import Capacitor
 class CapacitorGroup(LoadGroup):
     dss_module_name = 'Capacitors'
     ele_class = Capacitor
-
-    def get_cappu_matrix(self):
-        """
-        return 3 x n matrix of kvar values summed over Bus
-        columns indexed by Bus index
-        """
-        return self._get_attr_by_bus('cappu', orient='col')
