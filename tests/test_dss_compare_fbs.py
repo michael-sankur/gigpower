e45a9ce19d5bf2c9c90f302c3543bd39c60a9092
Elaine Laguerta <ellaguerta@lbl.gov>
2021-04-08 16:03:03 -0700

    Combine nr3 and fbs tests

--- tests/test_dss_compare_fbs.py
+++ tests/test_dss_compare_fbs.py
@@ -6,6 +6,7 @@
 
 from circuit_mapper.solution_dss import SolutionDSS
 from circuit_mapper.solution_fbs import SolutionFBS
+from circuit_mapper.solution_nr3 import SolutionNR3
 from circuit_mapper.pretty_print import compare_data_frames
 from circuit_mapper.circuit import Circuit
 
@@ -17,12 +18,12 @@ from pathlib import Path
 import pytest
 
 DSS_FILE_DIR = Path('./src/nr3_python/')
-OUT_DIR = Path('./tests/test_compare_fbs_dss')
-OUT_PREFIX = 'FBS_v_DSS_'
+OUT_DIR_FBS = Path('./tests/test_compare_fbs_dss')
+OUT_DIR_NR3 = Path('./tests/test_compare_nr3_dss')
 
 # thresholds for passing tests
-GENEROUS = 10e-2
-STRICT = 10e-3
+GENEROUS = .5
+STRICT = .01
 
 
 @pytest.fixture
@@ -33,7 +34,6 @@ def dss_solution(dss_file):
     return dss_solution
 
 
-@pytest.fixture
 def new_fbs_solution(dss_file):
     fp = str(Path(DSS_FILE_DIR, dss_file))
     solution = SolutionFBS(fp)
@@ -41,11 +41,27 @@ def new_fbs_solution(dss_file):
     return solution
 
 
+def new_nr3_solution(dss_file):
+    fp = str(Path(DSS_FILE_DIR, dss_file))
+    solution = SolutionNR3(fp)
+    solution.solve()
+    return solution
+
+
 def setup_module():
-    if not OUT_DIR.exists():
-        os.makedirs(OUT_DIR)
+    if not OUT_DIR_NR3.exists():
+        os.makedirs(OUT_DIR_NR3)
+    if not OUT_DIR_FBS.exists():
+        os.makedirs(OUT_DIR_FBS)
 
 
+@pytest.mark.parametrize(
+    "algorithm,out_dir",
+    [
+        ("NR3", OUT_DIR_NR3),
+        ("FBS", OUT_DIR_FBS),
+    ]
+)
 @pytest.mark.parametrize(
     "zip_values,zip_name",
     [
@@ -71,32 +87,39 @@ def setup_module():
     "solution_param",
     [(param) for param in SolutionFBS.SOLUTION_PARAMS]
 )
-def test_dss_v_new_fbs(new_fbs_solution, dss_solution, zip_values,
+def test_dss_v_new_fbs(algorithm, out_dir, dss_solution, zip_values,
                        zip_name, solution_param, dss_file, tolerance):
     """
     Compare the python FBS solution to the opendss solution.
     Writes output to OUTPUT FOLDER.
     """
+    if algorithm == 'NR3':
+        new_solution = new_nr3_solution(dss_file)
+    elif algorithm == 'FBS':
+        new_solution = new_fbs_solution(dss_file)
     Circuit.set_zip_values(zip_values)
     fp = Path(DSS_FILE_DIR, dss_file)
-    out_file = Path(OUT_DIR, OUT_PREFIX + str(fp.stem) + '_' +
+    out_prefix = f"{algorithm}_v_DSS_"
+    out_file = Path(out_dir, out_prefix + str(fp.stem) + '_' +
                     zip_name + '-' +
                     solution_param).with_suffix('.out.txt')
     sys.stdout = open(out_file, 'w')
-    fbs_vals = new_fbs_solution.get_data_frame(solution_param)
+    new_vals = new_solution.get_data_frame(solution_param)
     dss_vals = dss_solution.get_data_frame(solution_param)
+
+    # set sV pass threshold to 5% of opendss sV magnitude
     if solution_param == 'sV':
         dss_thresholds = dss_vals.abs() * .05
-        diffs = (fbs_vals.abs() - dss_vals.abs()).abs()
+        diffs = (new_vals.abs() - dss_vals.abs()).abs()
         test = ((diffs <= dss_thresholds).max()).all()
     else:
-        if solution_param == 'Vmag':
+        if solution_param == 'Vmag': # set Vmag tolerance to strict in all cases
             tolerance = STRICT
-        test = ((fbs_vals - dss_vals).abs().max() <= tolerance).all()
+        test = ((new_vals - dss_vals).abs().max() <= tolerance).all()
     if test:
-        print(f"TEST PASSED. FBS CONVERGED = {new_fbs_solution.converged}")
+        print(f"TEST PASSED. {algorithm} CONVERGED = {new_solution.converged}")
     else:
-        print(f"TEST FAILED. FBS CONVERGED = {new_fbs_solution.converged}")
+        print(f"TEST FAILED. {algorithm} CONVERGED = {new_solution.converged}")
     print(f"Zip values = {zip_values}")
-    compare_data_frames(fbs_vals, dss_vals, 'fbs', 'dss', solution_param)
+    compare_data_frames(new_vals, dss_vals, algorithm, 'dss', solution_param)
     assert test
