0dc977239f49bf694ef7510d36bf1571def329ae
Elaine Laguerta <ellaguerta@lbl.gov>
2021-06-10 14:30:27 -0700

    Add files for modules under src/ to be visible to pytest

---
+++ gigpower/src/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+setup(name="circuit_mapper", packages=find_packages())
+setup(name="fbs", packages=find_packages())
+setup(name="nr3", packages=find_packages())
